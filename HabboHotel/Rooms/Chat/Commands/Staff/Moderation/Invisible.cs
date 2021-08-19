6aed5f3ce6499d7bfb2bf9565c81cc7890078027
IseumaelPark <88250803+IseumaelPark@users.noreply.github.com>
2021-08-19 14:35:00 +0200

    Update : remove bubble invisible

--- HabboHotel/Rooms/Chat/Commands/Staff/Moderation/Invisible.cs
+++ HabboHotel/Rooms/Chat/Commands/Staff/Moderation/Invisible.cs
@@ -1,3 +1,4 @@
+using Butterfly.Communication.Packets.Outgoing;
 using Butterfly.Communication.Packets.Outgoing.Navigator;
 using Butterfly.HabboHotel.GameClients;
 
@@ -7,23 +8,17 @@ namespace Butterfly.HabboHotel.Rooms.Chat.Commands.Cmd
     {
         public void Execute(GameClient Session, Room Room, RoomUser UserRoom, string[] Params)
         {
+
             if (Session.GetHabbo().SpectatorMode)
             {
                 Session.GetHabbo().SpectatorMode = false;
                 Session.GetHabbo().HideInRoom = false;
-
-                //UserRoom.SendWhisperChat(ButterflyEnvironment.GetLanguageManager().TryGetValue("invisible.disabled", Session.Langue));
             }
             else
             {
                 Session.GetHabbo().SpectatorMode = true;
                 Session.GetHabbo().HideInRoom = true;
-
-                //UserRoom.SendWhisperChat(ButterflyEnvironment.GetLanguageManager().TryGetValue("invisible.enabled", Session.Langue));
             }
-
-            //UserRoom.SendWhisperChat(ButterflyEnvironment.GetLanguageManager().TryGetValue("invisible.waiting", Session.Langue));
-
             Session.SendPacket(new GetGuestRoomResultComposer(Session, Room.RoomData, false, true));
         }
     }
