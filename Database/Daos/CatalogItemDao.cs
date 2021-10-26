08218dbd1e4b811af1e2f5171ca4438b8ca69363
Kodamas <superjason95@gmail.com>
2021-10-26 19:39:25 +0200

    fix: update csproj

--- Database/Daos/CatalogItemDao.cs
+++ Database/Daos/CatalogItemDao.cs
@@ -12,15 +12,15 @@ namespace Butterfly.Database.Daos
             dbClient.RunQuery();
         }
 
-         internal static void Query8(IQueryAdapter dbClient)
+        internal static void Query8(IQueryAdapter dbClient)
         {
             dbClient.SetQuery("SELECT id,item_id,catalog_name,cost_credits,cost_pixels,cost_diamonds,amount,page_id,limited_sells,limited_stack,offer_active,badge FROM catalog_items ORDER by ID DESC");
             DataTable CatalogueItems = dbClient.GetTable();
         }
 
-          internal static void Query8(IQueryAdapter dbClient)
-    {
-        dbClient.RunQuery("SELECT item_id FROM catalog_items WHERE page_id IN (SELECT id FROM catalog_pages WHERE min_rank <= '" + Session.GetHabbo().Rank + "') AND cost_pixels = '0' AND cost_diamonds = '0' AND limited_sells = '0' AND limited_stack = '0' AND offer_active = '1' GROUP BY item_id");
-    }
+        internal static void Query8(IQueryAdapter dbClient)
+        {
+            dbClient.RunQuery("SELECT item_id FROM catalog_items WHERE page_id IN (SELECT id FROM catalog_pages WHERE min_rank <= '" + Session.GetHabbo().Rank + "') AND cost_pixels = '0' AND cost_diamonds = '0' AND limited_sells = '0' AND limited_stack = '0' AND offer_active = '1' GROUP BY item_id");
+        }
     }
 }
