ef0d84c8eb7e17ea7f4d8eca90c17dc4aba8dcd4
Iseumael Park <88250803+IseumaelPark@users.noreply.github.com>
2021-11-24 13:05:19 +0100

    fix: kick :kr:

--- Game/Rooms/Chat/Commands/Staff/Moderation/KickStaff.cs
+++ Game/Rooms/Chat/Commands/Staff/Moderation/KickStaff.cs
@@ -1,4 +1,14 @@
-using Butterfly.Game.Clients;namespace Butterfly.Game.Rooms.Chat.Commands.Cmd{    internal class KickStaff : IChatCommand    {        public void Execute(Client Session, Room Room, RoomUser UserRoom, string[] Params)        {            Room currentRoom = Session.GetHabbo().CurrentRoom;            Client clientByUsername = ButterflyEnvironment.GetGame().GetClientManager().GetClientByUsername(Params[1]);            if (clientByUsername == null || clientByUsername.GetHabbo() == null)
+using Butterfly.Game.Clients;
+
+namespace Butterfly.Game.Rooms.Chat.Commands.Cmd
+{
+    internal class KickStaff : IChatCommand
+    {
+        public void Execute(Client Session, Room Room, RoomUser UserRoom, string[] Params)
+        {
+            Room currentRoom = Session.GetHabbo().CurrentRoom;
+            Client clientByUsername = ButterflyEnvironment.GetGame().GetClientManager().GetClientByUsername(Params[1]);
+            if (clientByUsername == null || clientByUsername.GetHabbo() == null)
             {
                 Session.SendNotification(ButterflyEnvironment.GetLanguageManager().TryGetValue("input.usernotfound", Session.Langue));
             }
@@ -6,13 +16,30 @@ using Butterfly.Game.Clients;namespace Butterfly.Game.Rooms.Chat.Commands.Cmd
             {
                 Session.SendNotification(ButterflyEnvironment.GetLanguageManager().TryGetValue("action.notallowed", Session.Langue));
             }
-            else if (clientByUsername.GetHabbo().CurrentRoomId < 1U)            {                Session.SendNotification(ButterflyEnvironment.GetLanguageManager().TryGetValue("kick.error", Session.Langue));            }            else            {                Room.GetRoomUserManager().RemoveUserFromRoom(clientByUsername, true, false);                if (Params.Length > 2)                {                    string message = CommandManager.MergeParams(Params, 2);                    if (Session.Antipub(message, "<CMD>", Room.Id))
+            else if (clientByUsername.GetHabbo().CurrentRoomId < 1U)
+            {
+                Session.SendNotification(ButterflyEnvironment.GetLanguageManager().TryGetValue("kick.error", Session.Langue));
+            }
+            else
+            {
+                Room.GetRoomUserManager().RemoveUserFromRoom(clientByUsername, true, false);
+
+                if (Params.Length > 2)
+                {
+                    string message = CommandManager.MergeParams(Params, 2);
+                    if (Session.Antipub(message, "<CMD>", Room.Id))
                     {
                         return;
                     }
 
-                    clientByUsername.SendNotification(ButterflyEnvironment.GetLanguageManager().TryGetValue("kick.withmessage", clientByUsername.Langue) + message);                }                else
+                    clientByUsername.SendNotification(ButterflyEnvironment.GetLanguageManager().TryGetValue("kick.withmessage", clientByUsername.Langue) + message);
+                }
+                else
                 {
                     clientByUsername.SendNotification(ButterflyEnvironment.GetLanguageManager().TryGetValue("kick.nomessage", clientByUsername.Langue));
                 }
-            }        }    }}
+            }
+
+        }
+    }
+}
--- Game/Rooms/Chat/Commands/User/Kick.cs
+++ Game/Rooms/Chat/Commands/User/Kick.cs
@@ -7,40 +7,25 @@ namespace Butterfly.Game.Rooms.Chat.Commands.Cmd
     {
         public void Execute(Client Session, Room Room, RoomUser UserRoom, string[] Params)
         {
-            Room currentRoom = Session.GetHabbo().CurrentRoom;
+
             Client clientByUsername = ButterflyEnvironment.GetGame().GetClientManager().GetClientByUsername(Params[1]);
-            Room room = ButterflyEnvironment.GetGame().GetRoomManager().GetRoom(Session.GetHabbo().CurrentRoomId);
 
             if (clientByUsername == null || clientByUsername.GetHabbo() == null)
             {
-                Session.SendNotification(ButterflyEnvironment.GetLanguageManager().TryGetValue("input.usernotfound", Session.Langue));
+                return;
             }
-            else
+
+            if (Session.GetHabbo().Rank <= clientByUsername.GetHabbo().Rank)
             {
-                foreach (RoomUser User in currentRoom.GetRoomUserManager().GetUserList().ToList())
-                {
-                    if (User != null && !User.IsBot && !User.GetClient().GetHabbo().HasFuse("fuse_mod") && User.GetClient().GetHabbo().Id != Session.GetHabbo().Id)
-                    {
-                        User.AllowMoveTo = false;
-                        User.IsWalking = true;
-                        User.AllowOverride = true;
-                        User.GoalX = Room.GetGameMap().Model.DoorX;
-                        User.GoalY = Room.GetGameMap().Model.DoorY;
-                    }
-                }
-
-
-                room.SetTimeout(3000, () =>
-                {
-                    foreach (RoomUser User in currentRoom.GetRoomUserManager().GetUserList().ToList())
-                    {
-                        if (User != null && !User.IsBot && !User.GetClient().GetHabbo().HasFuse("fuse_mod") && User.GetClient().GetHabbo().Id != Session.GetHabbo().Id)
-                        {
-                            Room.GetRoomUserManager().RemoveUserFromRoom(clientByUsername, true, true);
-                        }
-                    }
-                });
+                return;
             }
+
+            if (clientByUsername.GetHabbo().CurrentRoomId < 1U)
+            {
+                return;
+            }
+
+            Room.GetRoomUserManager().RemoveUserFromRoom(clientByUsername, true, true);
         }
     }
 }
