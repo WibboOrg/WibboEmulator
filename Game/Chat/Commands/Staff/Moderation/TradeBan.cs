e08c6e3c22aa7543709b40e6b1749741be6f0959
IseumaelPark <88250803+IseumaelPark@users.noreply.github.com>
2022-04-25 14:20:34 +0200

    add: tradeban
    Co-Authored-By: Jason <5114733+JasonDhose@users.noreply.github.com>

---
+++ Game/Chat/Commands/Staff/Moderation/TradeBan.cs
@@ -0,0 +1,18 @@
+﻿using Butterfly.Game.Clients;
+using Butterfly.Game.Rooms;
+
+namespace Butterfly.Game.Chat.Commands.Cmd
+{
+    internal class TradeBan : IChatCommand
+    {
+        public void Execute(Client Session, Room Room, RoomUser UserRoom, string[] Params)
+        {
+
+            if (Params.Length == 1)
+            {
+                UserRoom.SendWhisperChat("Il semble que vous ayez oublié des valeurs? (jours ou pseudonyme)");
+                return;
+            }
+        }
+    }
+}
