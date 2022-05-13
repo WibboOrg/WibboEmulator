be7ebf23bfc9912f5a9dacfd635cabe917ee820d
Iseumael Park <88250803+ismaelpark@users.noreply.github.com>
2022-05-13 22:24:40 +0200

    Update SendHotelAlertEvent.cs

--- Communication/Packets/Incoming/WibboTool/SendHotelAlertEvent.cs
+++ Communication/Packets/Incoming/WibboTool/SendHotelAlertEvent.cs
@@ -25,20 +25,20 @@ namespace Butterfly.Communication.Packets.Incoming.WibboTool
             string Url = Packet.PopString();
             bool Preview = Packet.PopBoolean();
 
-            if (string.IsNullOrWhiteSpace(Message) || Message.Length > 1000 || Message.Length < 20)
+            if (string.IsNullOrWhiteSpace(Message) || Message.Length > 2000 || Message.Length < 50)
             {
                 return;
             }
 
             if (Preview)
             {
-                Session.SendPacket(new NotifAlertComposer("staff", "Message de test", Message, "Super !", 0, Url));
+                Session.SendPacket(new NotifAlertComposer("staff", "Message de prévisualisation", Message, "Super !", 0, Url));
                 return;
             }
 
             if (!string.IsNullOrWhiteSpace(Url))
             {
-                ButterflyEnvironment.GetGame().GetModerationManager().LogStaffEntry(Session.GetUser().Id, Session.GetUser().Username, 0, string.Empty, "hal", string.Format("WbTool hal: {0} : {1}", Url, Message));
+                ButterflyEnvironment.GetGame().GetModerationManager().LogStaffEntry(Session.GetUser().Id, Session.GetUser().Username, 0, string.Empty, "hal", string.Format("WibbbTool HaL: {0} : {1}", Url, Message));
 
                 if (!Url.StartsWith("https://wibbo.org") && !Url.StartsWith("https://www.facebook.com/WibboHotelFR") && !Url.StartsWith("https://twitter.com/WibboOrg") && !Url.StartsWith("https://instagram.com/wibboorg"))
                 {
@@ -56,7 +56,7 @@ namespace Butterfly.Communication.Packets.Incoming.WibboTool
                     return;
                 }
 
-                ButterflyEnvironment.GetGame().GetModerationManager().LogStaffEntry(Session.GetUser().Id, Session.GetUser().Username, Session.GetUser().CurrentRoom.Id, string.Empty, "eventha", string.Format("WbTool eventha: {0}", Message));
+                ButterflyEnvironment.GetGame().GetModerationManager().LogStaffEntry(Session.GetUser().Id, Session.GetUser().Username, Session.GetUser().CurrentRoom.Id, string.Empty, "eventha", string.Format("WibbobTool EventHa: {0}", Message));
                 if (Session.Antipub(Message, "<eventalert>"))
                 {
                     return;
@@ -64,7 +64,7 @@ namespace Butterfly.Communication.Packets.Incoming.WibboTool
 
                 if (!ButterflyEnvironment.GetGame().GetAnimationManager().AllowAnimation())
                 {
-                    Session.SendNotification("Merci d'attendre que l'animation en cours soit terminer");
+                    Session.SendNotification("Vous pourrez lancer l'alerte une fois que l'animation en cours sera terminé.");
                     return;
                 }
 
@@ -80,7 +80,7 @@ namespace Butterfly.Communication.Packets.Incoming.WibboTool
                     return;
                 }
 
-                ButterflyEnvironment.GetGame().GetClientManager().SendMessage(new NotifAlertComposer("staff", "Message des Staffs", Message, "Compris !", 0, ""));
+                ButterflyEnvironment.GetGame().GetClientManager().SendMessage(new NotifAlertComposer("staff", "Message de l'Équipe", Message, "Compris !", 0, ""));
             }
 
         }
