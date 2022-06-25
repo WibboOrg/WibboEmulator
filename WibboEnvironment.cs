8360aed88da8da88f9d0ffd5f7d1e732bcf46ddc
Kodamas <superjason95@gmail.com>
2022-06-25 02:30:46 +0200

    feat: new ASCII logo

--- WibboEnvironment.cs
+++ WibboEnvironment.cs
@@ -57,24 +57,79 @@ namespace WibboEmulator
 
             PatchDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "/";
 
-            Console.Title = "Butterfly Emulator";
+            Console.Title = "Wibbo Emulator";
 
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write(@" __        __  ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write(@"_   ");
             Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write(@"_       ");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write(@"_             ");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write(@"");
 
-            Console.WriteLine(@"  ____  _    _ _______ _______ ______ _____  ______ _  __     __ ");
-            Console.WriteLine(@" |  _ \| |  | |__   __|__   __|  ____|  __ \|  ____| | \ \   / / ");
-            Console.WriteLine(@" | |_) | |  | |  | |     | |  | |__  | |__) | |__  | |  \ \_/ /  ");
-            Console.WriteLine(@" |  _ <| |  | |  | |     | |  |  __| |  _  /|  __| | |   \   /   ");
-            Console.WriteLine(@" | |_) | |__| |  | |     | |  | |____| | \ \| |    | |____| |    ");
-            Console.WriteLine(@" |____/ \____/   |_|     |_|  |______|_|  \_\_|    |______|_|    ");
+            Console.WriteLine(@"");
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write(@" \ \      / / ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write(@"(_) ");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write(@"| |__   ");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write(@"| |__     ");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write(@"___  ");
+
+            Console.WriteLine(@"");
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write(@"  \ \ /\ / /  ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write(@"| | ");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write(@"| '_ \  ");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write(@"| '_ \   ");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write(@"/ _ \ ");
+
             Console.WriteLine("");
 
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write(@"   \ V  V /   ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write(@"| | ");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write(@"| |_) | ");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write(@"| |_) | ");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write(@"| (_) |");
 
-            Console.WriteLine("     Butterfly Emulator Edition Wibbo");
-            Console.WriteLine("     https://www.wibbo.org/");
-            Console.WriteLine("     Credits : Butterfly and Plus Emulator.");
-            Console.WriteLine("     - Jason Dhose");
+            Console.WriteLine(@"");
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write(@"    \_/\_/    ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("|_| ");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write("|_.__/  ");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write("|_.__/   ");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write(@"\___/ ");
+
             Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine("https://wibbo.org/");
+            Console.WriteLine("Credits : Butterfly and Plus Emulator.");
+            Console.WriteLine("-Wibbo Dev");
             Console.WriteLine("");
 
             Console.ForegroundColor = ConsoleColor.White;
