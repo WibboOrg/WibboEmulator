fe2a9db1f99cb0f84365750464d1fedde43bc5d5
Kodamas <superjason95@gmail.com>
2023-03-03 09:45:23 +0100

    fix: wired timer trigger

--- Games/Items/Wired/Triggers/TimerTrigger.cs
+++ Games/Items/Wired/Triggers/TimerTrigger.cs
@@ -9,6 +9,7 @@ using WibboEmulator.Games.Rooms.Wired;
 public class TimerTrigger : WiredTriggerBase, IWired, IWiredCycleable
 {
     public int DelayCycle => (this.IntParams.Count > 0) ? this.IntParams[0] : 0;
+    private int _skipCycleCount;
 
     public TimerTrigger(Item item, Room room) : base(item, room, (int)WiredTriggerType.TRIGGER_ONCE)
     {
@@ -17,10 +18,21 @@ public class TimerTrigger : WiredTriggerBase, IWired, IWiredCycleable
         this.IntParams.Add(0);
     }
 
-    public void OnResetTimer(object sender, EventArgs e) => this.RoomInstance.WiredHandler.RequestCycle(new WiredCycle(this, null, null));
+    public void OnResetTimer(object sender, EventArgs e)
+    {
+        this._skipCycleCount++;
+        this.RoomInstance.WiredHandler.RequestCycle(new WiredCycle(this, null, null));
+    }
 
     public bool OnCycle(RoomUser user, Item item)
     {
+        this._skipCycleCount--;
+
+        if (this._skipCycleCount > 0)
+        {
+            return false;
+        }
+
         this.RoomInstance.WiredHandler.ExecutePile(this.ItemInstance.Coordinate, null, null);
         return false;
     }
