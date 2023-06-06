3d33dda98d59d61ba418c0cb7df3a0a651b79c3b
Kodamas <superjason95@gmail.com>
2023-06-06 02:06:17 +0200

    feat: empty_items_all permission

--- Games/Chats/Commands/User/Inventory/EmptyItems.cs
+++ Games/Chats/Commands/User/Inventory/EmptyItems.cs
@@ -14,6 +14,11 @@ internal sealed class EmptyItems : IChatCommand
 
         var emptyAll = parameters.Length > 1 && parameters[1] == "all";
 
+        if (emptyAll && !session.User.HasPermission("empty_items_all"))
+        {
+            emptyAll = false;
+        }
+
         session.User.InventoryComponent.ClearItems(emptyAll);
         userRoom.SendWhisperChat(WibboEnvironment.GetLanguageManager().TryGetValue("empty.cleared", session.Langue));
     }
