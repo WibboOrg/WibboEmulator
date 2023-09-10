f7b94a2a392ca2981247e2933ffa7e62cce922b7
Kodamas <superjason95@gmail.com>
2023-09-10 11:28:10 +0200

    fix: allowed character name

--- WibboEnvironment.cs
+++ WibboEnvironment.cs
@@ -40,7 +40,7 @@ public static class WibboEnvironment
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
             'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
             '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-            '-', '.', '=', '?', '!', ':'
+            '-', '.', '=', '!', ':', '@'
         });
 
     public static DateTime ServerStarted { get; private set; }
