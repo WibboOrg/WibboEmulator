7463dd13c424d3e134cb99b5f54af7e45132ccd2
Kodamas <superjason95@gmail.com>
2024-08-28 06:09:30 +0200

    fix: badge manager default badge

--- Games/Badges/BadgeManager.cs
+++ Games/Badges/BadgeManager.cs
@@ -5,6 +5,7 @@ using WibboEmulator.Database.Daos.Emulator;
 
 public static class BadgeManager
 {
+    private static readonly string ACHIVEMENT_BADGE = "ACH_";
     private static readonly Dictionary<string, Badge> BadgeList = [];
 
     public static void Initialize(IDbConnection dbClient)
@@ -15,19 +16,26 @@ public static class BadgeManager
 
         foreach (var emulatorBadge in emulatorBadgeList)
         {
-            BadgeList.Add(emulatorBadge.Code, new Badge(emulatorBadge.Code, emulatorBadge.CanTrade, emulatorBadge.CanDelete, emulatorBadge.CanGive, emulatorBadge.AmountWinwins));
+            if (!BadgeList.ContainsKey(emulatorBadge.Code))
+            {
+                BadgeList.Add(emulatorBadge.Code, new Badge(emulatorBadge.Code, emulatorBadge.CanTrade, emulatorBadge.CanDelete, emulatorBadge.CanGive, emulatorBadge.AmountWinwins));
+            }
         }
+
+        BadgeList.Add("", new Badge("", false, true, true, 0));
     }
 
-    public static bool TryGetBadgeByCode(string code, out Badge badge) => BadgeList.TryGetValue(code, out badge);
+    public static bool IsAchivementBadge(string code) => code.StartsWith(ACHIVEMENT_BADGE);
 
-    public static bool CanGiveBadge(string badgeId) => TryGetBadgeByCode(badgeId, out var badge) && badge.CanGive;
+    public static bool TryGetBadgeByCode(string code, out Badge badge) => BadgeList.TryGetValue(code, out badge) || BadgeList.TryGetValue("", out badge);
 
-    public static bool CanTradeBadge(string badgeId) => TryGetBadgeByCode(badgeId, out var badge) && badge.CanTrade;
+    public static bool CanGiveBadge(string code) => TryGetBadgeByCode(code, out var badge) && badge.CanGive && !IsAchivementBadge(code);
 
-    public static bool CanDeleteBadge(string badgeId) => TryGetBadgeByCode(badgeId, out var badge) && badge.CanDelete;
+    public static bool CanTradeBadge(string code) => TryGetBadgeByCode(code, out var badge) && badge.CanTrade && !IsAchivementBadge(code);
 
-    public static int AmountWinwinsBadge(string badgeId) => TryGetBadgeByCode(badgeId, out var badge) ? badge.AmountWinwins : 0;
+    public static bool CanDeleteBadge(string code) => TryGetBadgeByCode(code, out var badge) && badge.CanDelete && !IsAchivementBadge(code);
+
+    public static int AmountWinwinsBadge(string code) => TryGetBadgeByCode(code, out var badge) ? badge.AmountWinwins : 0;
 
     public static List<Badge> Badges => [.. BadgeList.Values];
 }
